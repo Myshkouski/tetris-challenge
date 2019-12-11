100e7fe978ddadaf11f7f519daba6fd94b7087df
Alexei <alexeimyshkouski@gmail.com>
2019-12-11 14:43:50 +0300

    wip

--- layouts/default.js
+++ layouts/default.js
@@ -3,6 +3,7 @@ export default {
     head: {
         meta: [
             { charset: 'utf-8' },
+            { name: 'og:url', property: 'og:url', content: 'https://myshkouski.github.io/tetris-challenge/' },
             { name: 'og:image', property: 'og:image', content: 'https://myshkouski.github.io/tetris-challenge/qr-code.png' },
             { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=0' },
         ],

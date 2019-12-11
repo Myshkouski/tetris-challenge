542ae72c48d3cef55c22e81b038ac985341d66eb
Alexei <alexeimyshkouski@gmail.com>
2019-12-11 12:44:21 +0300

    alpha-4

--- layouts/default.js
+++ layouts/default.js
@@ -1,12 +1,12 @@
-export default {
-    data() {
-        return {}
-    },
 
+export default {
     head: {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=0' },
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: this.$router.base + 'favicon.ico' }
         ]
     }
 }
